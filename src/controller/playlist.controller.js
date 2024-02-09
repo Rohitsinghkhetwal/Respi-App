2f17df5ddf0d65dff8af9bf4222dc270f1aadf38
rohit <raj0eshsing0909@gmail.com>
2024-02-10 03:45:33 +0530

    added the DeleteVedioFromPlaylist controller

--- src/controller/playlist.controller.js
+++ src/controller/playlist.controller.js
@@ -2,7 +2,7 @@ import {playlists} from "../models/Playlists.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 
 const createPlaylist = asyncHandler(async(req, res) => {
@@ -93,4 +93,78 @@ const getUserPlaylists = asyncHandler(async(req, resp) => {
     
 })
 
-export { createPlaylist, AddVedioToPlaylist, getUserPlaylists };
+const getplayListById = asyncHandler(async(req, resp) => {
+    const { Id} = req.params;
+    if(!Id){
+        throw new ApiError(400, "Id does not exist!");
+    }
+
+    const result = await playlists.findById(Id);
+    return resp
+    .status(200)
+    .json(
+        new ApiResponse(200, result, "playList Fetched successfully !!!!")
+    )
+})
+
+const removeVediotoPlaylist = asyncHandler(async(req, resp) => {
+    const {playlistId, vedioId} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(playlistId) || !mongoose.Types.ObjectId.isValid(vedioId)){
+        throw new ApiError(401, "Vedios not found !")
+
+    }
+
+    const searchingId = await playlists.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(playlistId),
+        },
+      },
+      {
+        $match: {
+          videos: {
+            $elemMatch: {
+              $eq: new mongoose.Types.ObjectId(vedioId),
+            },
+          },
+        },
+      },
+    ]);
+   
+    const ID = searchingId[0];
+    const vidID = vedioId.toString();
+
+    const operation = ID.videos.map(item => item.toString());
+    const index = operation.indexOf(vidID);
+    
+
+    if(index !== -1){
+        operation.splice(index, 1)
+    }
+
+   const result = await playlists.findByIdAndUpdate(playlistId, {
+    $set: {
+        videos: operation
+    }
+
+   },
+   {
+    new: true
+   })
+    console.log("vedios are in the arrays", result);
+
+    return resp
+    .status(200)
+    .json(
+        new ApiResponse(200, result, "Vedio deleted successfully !!!")
+    )
+})
+
+export {
+  createPlaylist,
+  AddVedioToPlaylist,
+  getUserPlaylists,
+  getplayListById,
+  removeVediotoPlaylist,
+};
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { AddVedioToPlaylist, createPlaylist, getUserPlaylists } from "../controller/playlist.controller.js";
+import { AddVedioToPlaylist,  createPlaylist, getUserPlaylists, getplayListById, removeVediotoPlaylist } from "../controller/playlist.controller.js";
 import { verifyJwt } from "../Middleware/Auth.middleware.js";
 
 const router = Router();
 router.route("/createPlaylist").post(verifyJwt, createPlaylist);
 router.route("/add/:vedioId/:playlistId").patch(verifyJwt, AddVedioToPlaylist);
-router.route("/getUserPlaylist/:userID").get(verifyJwt, getUserPlaylists)
+router.route("/getUserPlaylist/:userID").get(verifyJwt, getUserPlaylists);
+
+router.route("/getplayList/:Id").get(verifyJwt, getplayListById);
+router
+  .route("/removeVedio/:playlistId/:vedioId")
+  .patch(verifyJwt, removeVediotoPlaylist);
 
 
 export default router;
